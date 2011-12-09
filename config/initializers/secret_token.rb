b090614a2598c7d343ceb0b837a2ef9cfa292f67
jracine <jungle__girl88@hotmail.com>
2011-12-09 17:08:12 +0800

    Login part done with railscast #250

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoginApp::Application.config.secret_token = '078cc20c52fab72f936ec8b1f4876b0b3d286d7c22dbdb99c40d5c3abc747627a8206c76bc5f204cbfb38ff1f36c0d346d5b75dd7a85b16fe0c1157ad2d98686'
